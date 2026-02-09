bf1f151936e3cb56a23f62fd1e03f0ea05aa86c2
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2026-02-09 16:56:35 +0530

    fix: force react 18 and clean vite build

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 

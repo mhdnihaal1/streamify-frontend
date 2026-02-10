5c86d5576e794057eaf9374df6893479f5047aea
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2026-02-10 12:55:17 +0530

    fix: force react 18 and clean vite build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
- import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+//  import react from '@vitejs/plugin-react'
 
- export default defineConfig({
+//  export default defineConfig({
+//   plugins: [react()],
+//   base: './',  
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
   plugins: [react()],
-  base: './',  
-})
-
-
+});
